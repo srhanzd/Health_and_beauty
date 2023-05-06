f0c73e6bf36a1b1f635c7eb1f534a3ad5f12acb2
srhanzd <srhanzd@gmail.com>
2023-05-06 09:57:11 +0300

    validation1

--- app/Http/Requests/ResetPasswordRequest.php
+++ app/Http/Requests/ResetPasswordRequest.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Requests;
 
+use App\Traits\GeneralTrait;
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class ResetPasswordRequest extends FormRequest
 {
+    use GeneralTrait;
     /**
      * Determine if the user is authorized to make this request.
      *
@@ -29,5 +33,12 @@ class ResetPasswordRequest extends FormRequest
             'password' => 'required|min:8',
             'password_confirm' => 'required|same:password'
         ];
+    }
+    public function failedValidation(Validator $validator)
+    {
+        // dd($validator->errors());
+
+
+        throw new HttpResponseException($this->returnError(000,$validator->errors()));
     }
 }
