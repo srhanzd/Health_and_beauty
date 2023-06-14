be346f96e76379e2fd43b2b9dc639685aeb15853
srhanzd <srhanzd@gmail.com>
2023-06-14 15:21:51 +0300

    Appointment pending

---
+++ app/Services/Appointment_services/Appointment_get_pending_service.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Services\Appointment_services;
+
+use App\Models\Appointment;
+use App\Traits\GeneralTrait;
+use Illuminate\Http\Request;
+
+class Appointment_get_pending_service
+{
+    use GeneralTrait;
+    public function pending(Request $request)
+    {
+
+        try {
+
+            $user=auth()->user();
+
+            $patient=$user->patient;
+
+            $appointments=$patient->appointments()
+                ->where('IsDeleted','=',0)
+                ->where('Status','=',0)
+                ->latest()->paginate(5);
+            return $this->returnData('appointments',$appointments
+                , 'Pending Appointments retrieved successfully.', $request->header('lang'));
+
+        } catch
+        (\Exception $e) {
+
+            return $this->returnError($e->getLine(), $e->getMessage(), $request->header('lang'));
+
+        }
+    }
+}
